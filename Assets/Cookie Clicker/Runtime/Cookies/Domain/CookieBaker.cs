b329d8130a0392bed04fb3d88bd8e018fa025ff4
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-03 17:44:25 +0100

    test: cookie baker empty case

---
+++ Assets/Cookie Clicker/Runtime/Cookies/Domain/CookieBaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cookie_Clicker.Runtime.Cookies.Domain
+{
+    public class CookieBaker
+    {
+        public readonly List<Building> buildings = new List<Building>();
+        
+        public int TotalCookies { get; private set; }
+
+        public void PassTime(TimeSpan delta)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Cookie Clicker/Tests/CookieBakerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cookie_Clicker.Runtime.Cookies.Domain;
+using NUnit.Framework;
+
+namespace Cookie_Clicker.Tests
+{
+    public class CookieBakerTests
+    {
+        [Test]
+        public void EmptyCase()
+        {
+            var baker = new CookieBaker();
+
+            baker.PassTime(TimeSpan.FromSeconds(1));
+            
+            Assert.That(baker.TotalCookies, Is.EqualTo(0));
+        }
+    }
+}

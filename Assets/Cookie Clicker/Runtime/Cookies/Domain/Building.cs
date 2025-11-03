d7b3344a3805f85b41aa345d2d39daef420e6946
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-03 17:32:29 +0100

    test: building empty case

---
+++ Assets/Cookie Clicker/Runtime/Cookies/Domain/Building.cs
@@ -0,0 +1,15 @@
+﻿namespace Cookie_Clicker.Runtime.Cookies.Domain
+{
+    public class Building
+    {
+        public int Production => _baseCPS * _quantity;
+
+        private readonly int _baseCPS;
+        private int _quantity;
+        
+        public Building(int baseCPS)
+        {
+            _baseCPS = baseCPS;
+        }
+    }
+}
---
+++ Assets/Cookie Clicker/Tests/BuildingTests.cs
@@ -0,0 +1,16 @@
+﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+using NUnit.Framework;
+
+namespace Cookie_Clicker.Tests
+{
+    public class BuildingTests
+    {
+        [Test]
+        public void EmptyCase()
+        {
+            var building = new Building(1);
+            
+            Assert.That(building.Production, Is.EqualTo(0));
+        }
+    }
+}

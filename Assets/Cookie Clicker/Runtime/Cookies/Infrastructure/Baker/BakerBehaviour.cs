5b752ff0db32a7a5489278d9a6e854dcb7a4f09a
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-05 16:15:07 +0100

    feat: click the cookie to obtain cookies

---
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/BakerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+using UnityEngine;
+
+namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
+{
+    public class BakerBehaviour : MonoBehaviour
+    {
+        [SerializeField] private ClickableCookie cookie;
+
+        private readonly CookieBaker _baker = new CookieBaker();
+        
+        private void OnEnable() => cookie.OnClick += Tap;
+        private void OnDisable() => cookie.OnClick -= Tap;
+
+        private void Tap()
+        {
+            _baker.Tap();
+            Debug.Log($"Cookies: {_baker.TotalCookies}");
+        }
+    }
+}
---
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/ClickableCookie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
+{
+    public class ClickableCookie : MonoBehaviour, IPointerClickHandler
+    {
+        public event Action OnClick = () => { };
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnClick.Invoke();
+        }
+    }
+}

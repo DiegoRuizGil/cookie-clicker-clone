74b438ad2647624c5eada2a9542444d858b006a7
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-05 12:55:19 +0100

    feat: percentage struct

---
+++ Assets/Cookie Clicker/Runtime/Cookies/Domain/Percentage.cs
@@ -0,0 +1,22 @@
+﻿namespace Cookie_Clicker.Runtime.Cookies.Domain
+{
+    public struct Percentage
+    {
+        private readonly float _value;
+
+        public float Normalized => _value;
+        public float AsPercentage => _value * 100f;
+
+        private Percentage(float normalizedValue)
+        {
+            _value = normalizedValue;
+        }
+        
+        public static Percentage FromFraction(float fraction) => new Percentage(fraction);
+        public static Percentage FromPercentage(float percentage) => new Percentage(percentage / 100f);
+        
+        public float AppliedTo(float baseValue) => baseValue * _value;
+        
+        public override string ToString() => $"{AsPercentage:0.##}%";
+    }
+}
---
+++ Assets/Cookie Clicker/Tests/PercentageTests.cs
@@ -0,0 +1,16 @@
+﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+using NUnit.Framework;
+
+namespace Cookie_Clicker.Tests
+{
+    public class PercentageTests
+    {
+        [Test]
+        public void ApplyPercentage()
+        {
+            var percentage = Percentage.FromPercentage(25f);
+            
+            Assert.That(percentage.AppliedTo(100f), Is.EqualTo(25f));
+        }
+    }
+}

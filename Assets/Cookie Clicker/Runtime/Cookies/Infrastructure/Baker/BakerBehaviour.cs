d540d8d67e5b70853837021a10566f43edcee214
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-05 16:19:19 +0100

    feat: show stats

--- Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/BakerBehaviour.cs
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/BakerBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+﻿using System;
+using Cookie_Clicker.Runtime.Cookies.Domain;
 using UnityEngine;
 
 namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
@@ -6,16 +7,22 @@ namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
     public class BakerBehaviour : MonoBehaviour
     {
         [SerializeField] private ClickableCookie cookie;
+        [SerializeField] private StatsVisualizer statsVisualizer;
 
         private readonly CookieBaker _baker = new CookieBaker();
-        
+
+        private void Start()
+        {
+            statsVisualizer.UpdateStats(_baker);
+        }
+
         private void OnEnable() => cookie.OnClick += Tap;
         private void OnDisable() => cookie.OnClick -= Tap;
 
         private void Tap()
         {
             _baker.Tap();
-            Debug.Log($"Cookies: {_baker.TotalCookies}");
+            statsVisualizer.UpdateStats(_baker);
         }
     }
 }
---
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/StatsVisualizer.cs
@@ -0,0 +1,18 @@
+﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+using TMPro;
+using UnityEngine;
+
+namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
+{
+    public class StatsVisualizer : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI totalCookiesText;
+        [SerializeField] private TextMeshProUGUI cpsText;
+
+        public void UpdateStats(CookieBaker baker)
+        {
+            totalCookiesText.text = $"{baker.TotalCookies} COOKIES";
+            cpsText.text = $"per seconds: {baker.Production}";
+        }
+    }
+}

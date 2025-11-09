3de1e55d707a834f992c8e03fdb4a031007ec4c3
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-09 12:38:03 +0100

    feat: add conditions to upgrades

--- Assets/Cookie Clicker/Runtime/Modifiers/Domain/Upgrade.cs
+++ Assets/Cookie Clicker/Runtime/Modifiers/Domain/Upgrade.cs
@@ -1,9 +1,19 @@
-﻿using Cookie_Clicker.Runtime.Cookies.Domain;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cookie_Clicker.Runtime.Cookies.Domain;
+using Cookie_Clicker.Runtime.Modifiers.Domain.Unlock_Conditions;
 
 namespace Cookie_Clicker.Runtime.Modifiers.Domain
 {
     public abstract class Upgrade
     {
+        private readonly List<IUnlockCondition> _unlockConditions = new List<IUnlockCondition>();
+        protected bool IsUnlocked;
+        
         public abstract void Apply(CookieBaker baker);
+
+        public void AddUnlockCondition(IUnlockCondition condition) => _unlockConditions.Add(condition);
+        public bool CanUnlock(CookieBaker baker) => _unlockConditions.All(condition => condition.IsMet(baker));
+        public void Unlock() => IsUnlocked = true;
     }
 }

795591b1e1d65587f76458f068f474b361c41c06
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-16 19:46:07 +0100

    feat: spawn cursors around the cookie

---
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/CursorsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
+{
+    public class CursorsController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform cursorPrefab;
+        
+        [Header("Settings")]
+        [SerializeField] private float radius = 150;
+        [SerializeField] private float offsetBetweenCircles = 20;
+        [SerializeField] private int cursorsPerCircle = 50;
+        
+        private readonly Stack<RectTransform> _cursors = new  Stack<RectTransform>();
+        
+        private float OffsetBetweenCursors => (360f / cursorsPerCircle) * Mathf.Deg2Rad;
+        private int _currentCursor;
+        private int _currentCircle;
+
+        private void Update()
+        {
+            DebugInputs();
+        }
+
+        private void DebugInputs()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+                AddCursor();
+            if (Input.GetKeyDown(KeyCode.F2))
+                RemoveCursor();
+        }
+
+        private void AddCursor()
+        {
+            var angle = OffsetBetweenCursors * _currentCursor;
+            var actualRadius = radius + _currentCircle * offsetBetweenCircles;
+            var pos = new Vector2(actualRadius * Mathf.Cos(angle), actualRadius * Mathf.Sin(angle));
+            
+            var cursor = Instantiate(cursorPrefab, transform);
+            cursor.anchoredPosition = pos;
+            _cursors.Push(cursor);
+            
+            _currentCursor++;
+            if (_currentCursor >= cursorsPerCircle)
+            {
+                _currentCursor = 0;
+                _currentCircle++;
+            }
+        }
+
+        private void RemoveCursor()
+        {
+            if (_currentCursor <= 0 && _currentCircle <= 0) return;
+            
+            var cursor = _cursors.Pop();
+            Destroy(cursor.gameObject);
+
+            _currentCircle = _cursors.Count / cursorsPerCircle;
+            _currentCursor = _cursors.Count % cursorsPerCircle;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+}

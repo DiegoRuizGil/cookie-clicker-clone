bf6df1b399ed1cb7e76af164f771e105321a6bcc
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-17 16:34:14 +0100

    feat: tooltip data models

---
+++ Assets/Cookie Clicker/Runtime/Store/Infrastructure/Tooltip/TooltipModels.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Cookie_Clicker.Runtime.Store.Infrastructure.Tooltip
+{
+    public struct BuildingTooltipData
+    {
+        public Sprite icon;
+        public string name;
+        public int amount;
+        public float cost;
+        public float cpsPer;
+        public float totalProduction;
+        public float totalBaked;
+    }
+
+    public struct UpgradeTooltipData
+    {
+        public Sprite icon;
+        public string name;
+        public int cost;
+        public string description;
+    }
+}

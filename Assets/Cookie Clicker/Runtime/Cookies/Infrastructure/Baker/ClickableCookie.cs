a35f8a6da4b9d93bdba996b23a39505ddf5bef8e
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-11-19 17:15:30 +0100

    feat: avoid clicking outside the cookie

--- Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/ClickableCookie.cs
+++ Assets/Cookie Clicker/Runtime/Cookies/Infrastructure/Baker/ClickableCookie.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Cookie_Clicker.Runtime.Cookies.Infrastructure.Baker
 {
     public class ClickableCookie : MonoBehaviour, IPointerClickHandler
     {
         public event Action OnClick = () => { };
-        
+
+        private void Awake()
+        {
+            GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
             OnClick.Invoke();
---
+++ Assets/Cookie Clicker/Runtime/Store/Infrastructure/MouseDetectionTests.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Cookie_Clicker.Runtime.Store.Infrastructure
+{
+    public class MouseDetectionTests : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.Log("On Pointer Enter");
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Debug.Log("On Pointer Exit");
+        }
+    }
+}

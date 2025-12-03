e649ef304f99c992d5b63b238e9e12957f2acea0
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-12-03 12:28:39 +0100

    feat: sections toolbar

--- Assets/Cookie Clicker/Runtime/Tools/Editor/CookieAssetsTool.cs
+++ Assets/Cookie Clicker/Runtime/Tools/Editor/CookieAssetsTool.cs
@@ -5,12 +5,19 @@ namespace Cookie_Clicker.Runtime.Tools.Editor
 {
     public class CookieAssetsTool : EditorWindow
     {
+        private enum ToolSection
+        {
+            Settings, Buildings, Upgrades
+        }
+        
         [MenuItem("Tools/Cookie Assets")]
         public static void OpenWindow() => GetWindow<CookieAssetsTool>("Cookie Assets");
 
         private const string FolderPath = "Assets/Cookie Clicker/Data";
 
         private BuildingToolModule _buildingModule;
+        
+        private ToolSection _currentToolSection = ToolSection.Buildings;
 
         private void OnEnable()
         {
@@ -19,17 +26,34 @@ namespace Cookie_Clicker.Runtime.Tools.Editor
 
         private void OnGUI()
         {
-            GUILayout.BeginArea(new Rect(0, 0, position.width, position.height - 20));
             
-            _buildingModule.OnGUI();
+            DrawTabs();
 
+            switch (_currentToolSection)
+            {
+                case ToolSection.Settings:
+                    break;
+                case ToolSection.Buildings:
+                    _buildingModule.OnGUI();
+                    break;
+                case ToolSection.Upgrades:
+                    break;
+            }
+            
             if (Event.current.type == EventType.MouseDown)
             {
                 GUI.FocusControl(null);
                 Repaint();
             }
+        }
+
+        private void DrawTabs()
+        {
+            var tabs = new string[] { "Settings", "Buildings", "Upgrades" };
             
-            GUILayout.EndArea();
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            _currentToolSection = (ToolSection)GUILayout.Toolbar((int)_currentToolSection, tabs, GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
         }
     }
 }
